767efadaf20035cd5411a8299f24b8851ac1faa8
zhangbaoyi <909908771@qq.com>
2024-09-18 14:33:15 +0800

    test

--- resources/ExtensionContext.ts
+++ resources/ExtensionContext.ts
@@ -1,6 +1,13 @@
+/*
+ * @Description:
+ * @Author: zby
+ * @Date: 2024-09-18 14:18:09
+ * @LastEditors: zby
+ * @Reference:
+ */
 // Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ExtensionContext, Uri } from "vscode";
+import { ExtensionContext, Uri } from 'vscode';
 
 let context: ExtensionContext;
 
@@ -11,29 +18,20 @@ export function setContext(c: ExtensionContext) {
 /**
  * Set an extension context value.
  */
-export async function setContextValue(
-  key: string,
-  value: string,
-): Promise<void> {
+export async function setContextValue(key: string, value: string): Promise<void> {
   context.workspaceState.update(key, value);
 }
 
 /**
  * Get an extension context value.
  */
-export async function getContextValue(
-  key: string,
-): Promise<string | undefined> {
+export async function getContextValue(key: string): Promise<string | undefined> {
   return context.workspaceState.get(key);
 }
 
 export function getGlobalStorageUri(): Uri {
   console.log(context.globalStorageUri.fsPath);
-  const customUrl =
-    process.env.TEMP +
-    "\\" +
-    "globalStorage" +
-    context.globalStorageUri.fsPath.split("globalStorage").at(-1);
+  const customUrl = process.env.TEMP + '\\' + 'globalStorage' + context.globalStorageUri.fsPath.split('globalStorage').at(-1);
   return Uri.file(customUrl);
   // return context.globalStorageUri;
 }
